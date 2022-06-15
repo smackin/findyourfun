1c61f8d43e9d247dae81a252b6066ad59da729c6
Stephanie <smmackin12@gmail.com>
2022-06-15 12:32:21 -0400

    second commit

---
+++ findyourfun.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/MFYof3
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "user" (
+    "userID" serial   NOT NULL,
+    "APIKey" int   NOT NULL,
+    "Name" string   NOT NULL,
+    "UserName" string   NOT NULL,
+    "Password" string   NOT NULL,
+    "email" string   NOT NULL,
+    "State" string(2)   NOT NULL,
+    CONSTRAINT "pk_user" PRIMARY KEY (
+        "userID"
+     )
+);
+
+CREATE TABLE "parks" (
+    "parksid" serial   NOT NULL,
+    "Name" string   NOT NULL,
+    "location" int   NOT NULL,
+    "url" text   NOT NULL,
+    "activities" int   NOT NULL,
+    CONSTRAINT "pk_parks" PRIMARY KEY (
+        "parksid"
+     )
+);
+
+CREATE TABLE "activities" (
+    "activityID" serial   NOT NULL,
+    "name" text   NOT NULL,
+    "location" text   NOT NULL,
+    "parksid" int   NOT NULL,
+    CONSTRAINT "pk_activities" PRIMARY KEY (
+        "activityID"
+     )
+);
+
+CREATE TABLE "favorites" (
+    "faveID" serial   NOT NULL,
+    "userID" int   NOT NULL,
+    "parksid" int   NOT NULL,
+    "userId" INTEGER   NOT NULL,
+    CONSTRAINT "pk_favorites" PRIMARY KEY (
+        "faveID"
+     )
+);
+
+ALTER TABLE "activities" ADD CONSTRAINT "fk_activities_parksid" FOREIGN KEY("parksid")
+REFERENCES "parks" ("parksid");
+
+ALTER TABLE "favorites" ADD CONSTRAINT "fk_favorites_parksid" FOREIGN KEY("parksid")
+REFERENCES "parks" ("parksid");
+
+ALTER TABLE "favorites" ADD CONSTRAINT "fk_favorites_userID" FOREIGN KEY("userID")
+REFERENCES "user" ("userID");
+
+CREATE INDEX "idx_user_Name"
+ON "user" ("Name");
+
